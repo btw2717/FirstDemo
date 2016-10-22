caf9703d6454af1b2da3c66f4c47d4bb085c9fb6
kl2723 <enamyk@gmail.com>
2016-10-22 15:25:33 -0400

    Java Program
    ...

---
+++ AverageProgram.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class AverageProgram{
+
+public static void main(String[]args){
+
+Scanner keyboard=new Scanner(System.in);
+
+System.out.println("Enter 3 numbers and I will find average.");
+
+int num1=keyboard.nextInt();
+int num2=keyboard.nextInt();
+int num3=keyboard.nextInt();
+
+
+//Make code to find average
+
+
+
+
+}
+
+}
